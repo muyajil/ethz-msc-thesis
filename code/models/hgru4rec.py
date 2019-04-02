e6f61406a9ddbc5dccdaf6241d8ef4c38cc84596
Mohammed Ajil <mohammed@ajil.ch>
2019-04-02 15:25:14 +0200

    model base

--- code/models/hgru4rec.py
+++ code/models/hgru4rec.py
@@ -1,14 +1,19 @@
 import tensorflow as tf
-from tensorflow import keras
-from keras import backend as K
+from tensorflow.contrib.cudnn_rnn import CudnnGRU
 
 
 def model_fn(features, labels, mode, params):
 
-    item_embeddings = keras.layers.Embedding(
-        params['num_products'],
-        params['embedding_dim']
-    )
+    item_embeddings = tf.feature_column.categorical_column_with_hash_bucket(
+        key='ProductId',
+        hash_bucket_size=params['num_products'])
     
-    rnn_cell = keras.layers.GRUCell()
-    session_rnn = keras.layers.RNN()
+    session_rnn = CudnnGRU(
+        num_layers=params['num_layers_session'],
+        num_units=params['num_units_session'],
+        name='session_rnn')
+
+    user_rnn = CudnnGRU(
+        num_layers=params['num_layers_user'],
+        num_units=params['num_units_user'],
+        name='user_rnn')
