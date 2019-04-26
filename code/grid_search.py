a204c12b0687ff592a4f4d350dff753d4c67219c
Mohammed Ajil <mohammed@ajil.ch>
2019-04-26 18:05:11 +0200

    added docker command and params

--- code/grid_search.py
+++ code/grid_search.py
@@ -1,6 +1,43 @@
 import argparse
 import json
 import subprocess
+from itertools import product
+
+docker_command = "DATASET_NAME=<DATASET_NAME> && \
+LEARNING_RATE=<LEARNING_RATE> && \
+BATCH_SIZE=<BATCH_SIZE> && \
+LOSS_FUNCTION=<LOSS_FUNCTION> && \
+OPTIMIZER=<OPTIMIZER> && \
+MODEL_VARIANT=<MODEL_VARIANT> && \
+\
+docker restart tensorboard && \
+docker pull eu.gcr.io/machinelearning-prod/ma_muy_models:$MODEL_VARIANT && \
+docker run \
+    -d \
+    --name=$LOSS_FUNCTION'_'$OPTIMIZER'_bs_'$BATCH_SIZE'_lr_'$LEARNING_RATE \
+    --runtime=nvidia \
+    --cpus=$(nproc) \
+    -v ~/logs:/logs \
+    eu.gcr.io/machinelearning-prod/ma_muy_models:<MODEL_VARIANT> \
+    python /code/hgru4rec/hgru4rec_trainer.py \
+        --train_prefix='gs://ma-muy/03_datasets/'$DATASET_NAME'/05_train/' \
+        --eval_prefix='gs://ma-muy/03_datasets/'$DATASET_NAME'/06_eval/' \
+        --batch_size=$BATCH_SIZE \
+        --session_rnn_units=100 \
+        --user_rnn_units=100 \
+        --num_products=596 \
+        --num_users=1089 \
+        --log_dir='/logs/'$MODEL_VARIANT'/'$DATASET_NAME'/'$LOSS_FUNCTION'/'$OPTIMIZER'/bs_'$BATCH_SIZE'/lr_'$LEARNING_RATE'/' \
+        --epochs=1000000 \
+        --user_dropout=0.0 \
+        --session_dropout=0.1 \
+        --init_dropout=0.0 \
+        --learning_rate=$LEARNING_RATE \
+        --clip_gradients_at=10 \
+        --momentum=0.5 \
+        --loss_function=$LOSS_FUNCTION \
+        --optimizer=$OPTIMIZER \
+        --train_steps=200000"
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -13,4 +50,26 @@ if __name__ == '__main__':
 
     config = json.load(open(args.config_path))
 
-    subprocess.run(["docker", "pull", "eu.gcr.io/machinelearning-prod/ma_muy_models:combined"])
+    values = [
+        config["dataset_names"],
+        config["loss_functions"],
+        config["optimizers"],
+        config["batch_sizes"],
+        config["learning_rates"]
+    ]
+
+    for (dataset_name,
+            loss_function,
+            optimizer,
+            batch_size,
+            learning_rate) in product(*values):
+
+        # TODO: Set env vars and run command
+
+        print(dataset_name,
+              loss_function,
+              optimizer,
+              batch_size,
+              learning_rate)
+
+    # subprocess.run(["docker", "pull", "eu.gcr.io/machinelearning-prod/ma_muy_models:combined"])
