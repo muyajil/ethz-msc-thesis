dcc51bbc1e4f19659701f0e96ab1a94fd9853508
Mohammed Ajil <mohammed@ajil.ch>
2019-05-16 10:37:44 +0200

    Implemented prediction API

--- code/session_recommendation/main.py
+++ code/session_recommendation/main.py
@@ -1,11 +1,16 @@
 from flask import Flask, jsonify, request
-from hgru4rec.hgru4rec import model_fn as hgru4rec_model_fn
+from session_recommendation.hgru4rec.hgru4rec import model_fn as hgru4rec_model_fn
 import json
+import tensorflow as tf
+from dg_ml_core.datastores import gcs_utils
+from dg_ml_core.collections import dict_ops
+import argparse
+
 
 app = Flask('SessionBasedRecommendations')
 READY = False
 MODEL = None
-EMBEDDING_DICT = None
+EMBEDDING_DICT = dict()
 
 
 @app.route('/Readiness/', methods=['GET'])
@@ -20,17 +25,20 @@ def home():
     return jsonify({"message": "Session Recommendation App is live"}), 200
 
 
-def initialize_app(model_name, model_path, embedding_dict_path, params):
+def initialize_app(model_name, model_path, embedding_dict_path, params_path):
     global READY
     global MODEL
     global EMBEDDING_DICT
-    # TODO: does gcs work or is it necessary to have the model in the image?
-    # TODO: params loaded from local/remote json -> maybe save them after training in the model dir?
-    # TODO: Load embedding dict
+
+    EMBEDDING_DICT = dict_ops.load_dict(embedding_dict_path)
+    params = dict_ops.load_dict(params_path)
+
+    gcs_utils.download_folder_to_target(model_path, '/model_data')
+
     if model_name == 'hgru4rec':
         MODEL = tf.estimator.Estimator(
             model_fn=hgru4rec_model_fn,
-            model_dir=model_path,
+            model_dir='/model_data',
             params=params)
     else:
         raise RuntimeError(f'Model {model_name} unknown')
@@ -43,20 +51,40 @@ def predict():
         request_data = request.get_json()
         user_id = request_data['userId']
         product_id = request_data['productId']
+        session_start = request_data['sessionStart']
 
-        if (str(user_id) not in EMBEDDING_DICT['User']['ToEmbedding'] or
-                str(product_id) not in EMBEDDING_DICT['Product']['ToEmbedding']):
+        if str(user_id) not in EMBEDDING_DICT['User']['ToEmbedding']:
             return (jsonify({
                 "error": {
                     "code": 404,
-                    "message": "User or product is not supported"
+                    "message": "User is not supported"
                 }}), 404)
 
+        if str(product_id) not in EMBEDDING_DICT['Product']['ToEmbedding']:
+            return (jsonify({
+                "error": {
+                    "code": 404,
+                    "message": "Product is not supported"
+                }}), 404)
+            
         else:
-            # TODO: build input features (user changed always if session starts)
-            # TODO: get embedding ids
-            # TODO: predict result
-            pass
+            features = dict()
+            features['UserId'] = [user_id]
+            features['UserEmbeddingId'] = [EMBEDDING_DICT['User']['ToEmbedding'][str(user_id)]]
+            features['ProductId'] = product_id
+            features['EmbeddingId'] = [EMBEDDING_DICT['Product']['ToEmbedding'][str(product_id)]]
+            features['SessionChanged'] = int(session_start)
+            features['UserChanged'] = 0
+            features['Epoch'] = 0
+            features['LastSessionEvent'] = 1
+
+            predictions = MODEL.predict(features)
+
+            product_ids = map(lambda x: EMBEDDING_DICT['Prodct']['FromEmbedding'][str(x)], predictions)
+
+            return (jsonify({
+                'predictions': list(product_ids)
+            }), 200)
 
     else:
         return (jsonify({
@@ -66,8 +94,15 @@ def predict():
             }}), 400)
 
 
-initialize_app()
-
-
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_name', type=str)
+    parser.add_argument('--model_path', type=str)
+    parser.add_argument('--embedding_dict_path', type=str)
+    parser.add_argument('--params_path', type=str)
+
+    args = parser.parse_args()
+
     app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
+
+    initialize_app(**args)
