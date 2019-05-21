51bba00e0491a0d367dfec5486baf884b56ffc2f
Mohammed Ajil <mohammed@ajil.ch>
2019-05-21 12:35:17 +0200

    initialize app from args

--- code/session_recommendation/wsgi.py
+++ code/session_recommendation/wsgi.py
@@ -1,6 +1,15 @@
 from session_recommendation.main import app, initialize_app
+import argparse
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_name', type=str)
+    parser.add_argument('--model_path', type=str)
+    parser.add_argument('--embedding_dict_path', type=str)
+    parser.add_argument('--params_path', type=str)
+
     app.run()
-    # initialize_app() 
-    # TODO: Read config from json file and initialize app using thiss
+
+    args = parser.parse_args()
+    initialize_app(args.model_name, args.model_path, args.embedding_dict_path, args.params_path)
