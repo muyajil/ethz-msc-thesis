d13d0d82de255b40d7ff023ecb5ef6118927ea2f
Mohammed Ajil <mohammed@ajil.ch>
2019-08-14 20:59:56 +0200

    do not delete bibliography

--- writeup/build.sh
+++ writeup/build.sh
@@ -5,7 +5,7 @@ bibtex thesis
 pdflatex -synctex=1 -interaction=nonstopmode thesis.tex
 pdflatex -synctex=1 -interaction=nonstopmode thesis.tex
 rm thesis.aux
-rm thesis.bbl
+#rm thesis.bbl
 rm thesis.blg
 rm thesis.dvi
 rm thesis.out
