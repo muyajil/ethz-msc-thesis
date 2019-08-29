3b443f55b0db5d96df5ad331977b6e7fc2fbeb0a
Mohammed Ajil <mohammed@ajil.ch>
2019-08-29 12:53:53 +0200

    working on background section

--- writeup/build.sh
+++ writeup/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 pdflatex -synctex=1 -interaction=nonstopmode thesis.tex
+rm thesis.bbl
 bibtex thesis
 pdflatex -synctex=1 -interaction=nonstopmode thesis.tex
 pdflatex -synctex=1 -interaction=nonstopmode thesis.tex
 rm thesis.aux
-#rm thesis.bbl
 rm thesis.blg
 rm thesis.dvi
 rm thesis.out
