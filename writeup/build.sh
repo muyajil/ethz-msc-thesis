2f013d99577887440203568b852b4af4ddfe03fe
Mohammed <mohammed@ajil.ch>
2019-09-10 23:55:23 +0200

    new build script using latexdocker

--- writeup/build.sh
+++ writeup/build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-pdflatex -synctex=1 -interaction=nonstopmode thesis.tex &> /dev/null
+latexdocker pdflatex -synctex=1 -interaction=nonstopmode thesis.tex &> /dev/null
 rm thesis.bbl
-bibtex thesis
-pdflatex -synctex=1 -interaction=nonstopmode thesis.tex &> /dev/null
-pdflatex -synctex=1 -interaction=nonstopmode thesis.tex
+latexdocker bibtex thesis
+latexdocker pdflatex -synctex=1 -interaction=nonstopmode thesis.tex &> /dev/null
+latexdocker pdflatex -synctex=1 -interaction=nonstopmode thesis.tex
 rm thesis.aux
 rm thesis.blg
-# rm thesis.dvi
 rm thesis.out
 rm thesis.thm
 rm thesis.toc
-# rm thesis.synctex.gz
